58379e73e3552d74376804a6c2a5135899d9ccf2
Nozomi Ito <nozomi.itotrident-qa.com>
2013-10-04 10:49:42 +0900

    Now the test code works well for the STAR hotel web sight before and after the renewal (not tested for after the renewal)

--- answer/practicework/work4/pages/ReserveInputPage.java
+++ answer/practicework/work4/pages/ReserveInputPage.java
@@ -15,7 +15,7 @@ public class ReserveInputPage {
     private By breakfastOff = By.id("breakfast_off");
     private By planA = By.id("plan_a");
     private By planB = By.id("plan_b");
-    private By reservePersonName = By.id("reserve_person_name");
+    private By guestName = By.id("guestname");
     private By goToNext = By.id("goto_next");
 
     public ReserveInputPage(WebDriver driver) {
@@ -29,7 +29,7 @@ public class ReserveInputPage {
     }
     
     public String getReserveYear() {
-        return driver.findElement(reserveYear).getText();
+        return driver.findElement(reserveYear).getAttribute("value");
     }
 
     public void setReserveMonth(String value) {
@@ -39,7 +39,7 @@ public class ReserveInputPage {
     }
     
     public String getReserveMonth() {
-        return driver.findElement(reserveMonth).getText();
+        return driver.findElement(reserveMonth).getAttribute("value");
     }
     
     public void setReserveDay(String value) {
@@ -49,7 +49,7 @@ public class ReserveInputPage {
     }
     
     public String getReserveDay() {
-        return driver.findElement(reserveDay).getText();
+        return driver.findElement(reserveDay).getAttribute("value");
     }
 
     public void setReserveDate(String year, String month, String day) {
@@ -65,7 +65,7 @@ public class ReserveInputPage {
     }
     
     public String getReserveTerm() {
-        return driver.findElement(reserveTerm).getText();
+        return driver.findElement(reserveTerm).getAttribute("value");
     }
     
     public void setHeadCount(String value) {
@@ -75,7 +75,7 @@ public class ReserveInputPage {
     }
     
     public String getHeadCount() {
-        return driver.findElement(headCount).getText();
+        return driver.findElement(headCount).getAttribute("value");
     }
     
     public void setBreakfast(boolean on) {
@@ -113,146 +113,16 @@ public class ReserveInputPage {
         return driver.findElement(planB).isSelected();
     }
 
-    public void setReservePersonName(String value) {
-        WebElement element = driver.findElement(reservePersonName);
+    public void setGuestName(String value) {
+        WebElement element = driver.findElement(guestName);
         element.clear();
         element.sendKeys(value);
     }
     
-    public String getReservePersonName() {
-        return driver.findElement(reservePersonName).getText();
+    public String getGuestName() {
+        return driver.findElement(guestName).getAttribute("value");
     }
-    package practicework.pages;
-
-    import org.openqa.selenium.By;
-    import org.openqa.selenium.WebDriver;
-    import org.openqa.selenium.WebElement;
-
-    public class ReserveInputPage {
-        private WebDriver driver;
-        private By reserveYear = By.id("reserve_year");
-        private By reserveMonth = By.id("reserve_month");
-        private By reserveDay = By.id("reserve_day");
-        private By reserveTerm = By.id("reserve_term");
-        private By headCount = By.id("headcount");
-        private By breakfastOn = By.id("breakfast_on");
-        private By breakfastOff = By.id("breakfast_off");
-        private By planA = By.id("plan_a");
-        private By planB = By.id("plan_b");
-        private By guestName = By.id("guestname");
-        private By goToNext = By.id("goto_next");
-
-        public ReserveInputPage(WebDriver driver) {
-            this.driver = driver;
-        }
-
-        public void setReserveYear(String value) {
-            WebElement element = driver.findElement(reserveYear);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getReserveYear() {
-            return driver.findElement(reserveYear).getAttribute("value");
-        }
-
-        public void setReserveMonth(String value) {
-            WebElement element = driver.findElement(reserveMonth);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getReserveMonth() {
-            return driver.findElement(reserveMonth).getAttribute("value");
-        }
-        
-        public void setReserveDay(String value) {
-            WebElement element = driver.findElement(reserveDay);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getReserveDay() {
-            return driver.findElement(reserveDay).getAttribute("value");
-        }
-
-        public void setReserveDate(String year, String month, String day) {
-            setReserveYear(year);
-            setReserveMonth(month);
-            setReserveDay(day);
-        }
-
-        public void setReserveTerm(String value) {
-            WebElement element = driver.findElement(reserveTerm);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getReserveTerm() {
-            return driver.findElement(reserveTerm).getAttribute("value");
-        }
-        
-        public void setHeadCount(String value) {
-            WebElement element = driver.findElement(headCount);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getHeadCount() {
-            return driver.findElement(headCount).getAttribute("value");
-        }
-        
-        public void setBreakfast(boolean on) {
-            if (on) {
-                driver.findElement(breakfastOn).click();
-            } else {
-                driver.findElement(breakfastOff).click();            
-            }
-        }
-        
-        public boolean getBreakfast() {
-            String checked = driver.findElement(breakfastOn).getAttribute("checked");
-            return "true".equals(checked);
-        }
-        
-        public void setPlanA(boolean checked) {
-            WebElement element = driver.findElement(planA);
-            if (element.isSelected() != checked) {
-                element.click();
-            }
-        }
-        
-        public boolean getPlanA() {
-            return driver.findElement(planA).isSelected();
-        }
-
-        public void setPlanB(boolean checked) {
-            WebElement element = driver.findElement(planB);
-            if (element.isSelected() != checked) {
-                element.click();
-            }
-        }
-
-        public boolean getPlanB() {
-            return driver.findElement(planB).isSelected();
-        }
-        
-        public void setGuestName(String value) {
-            WebElement element = driver.findElement(guestName);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getGuestName() {
-            return driver.findElement(guestName).getAttribute("value");
-        }
-        
-        public ReserveConfirmPage goToNext() {
-            driver.findElement(goToNext).click();
-            return new ReserveConfirmPage(driver);
-        }
-    }
-
+    
     public ReserveConfirmPage goToNext() {
         driver.findElement(goToNext).click();
         return new ReserveConfirmPage(driver);
--- answer/practicework/work6/pages/ReserveInputPage.java
+++ answer/practicework/work6/pages/ReserveInputPage.java
@@ -3,9 +3,11 @@ package practicework.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class ReserveInputPage {
     private WebDriver driver;
+    private By datePick = By.id("datePick");
     private By reserveYear = By.id("reserve_year");
     private By reserveMonth = By.id("reserve_month");
     private By reserveDay = By.id("reserve_day");
@@ -15,67 +17,47 @@ public class ReserveInputPage {
     private By breakfastOff = By.id("breakfast_off");
     private By planA = By.id("plan_a");
     private By planB = By.id("plan_b");
-    private By reservePersonName = By.id("reserve_person_name");
-    private By goToNext = By.id("goto_next");
+    private By guestName = By.id("guestname");
+    private By goToNext = By.id("agree_and_goto_next");
 
     public ReserveInputPage(WebDriver driver) {
         this.driver = driver;
     }
-
-    public void setReserveYear(String value) {
-        WebElement element = driver.findElement(reserveYear);
-        element.clear();
-        element.sendKeys(value);
-    }
     
     public String getReserveYear() {
-        return driver.findElement(reserveYear).getText();
+        return driver.findElement(reserveYear).getAttribute("value");
     }
 
-    public void setReserveMonth(String value) {
-        WebElement element = driver.findElement(reserveMonth);
-        element.clear();
-        element.sendKeys(value);
-    }
-    
     public String getReserveMonth() {
-        return driver.findElement(reserveMonth).getText();
-    }
-    
-    public void setReserveDay(String value) {
-        WebElement element = driver.findElement(reserveDay);
-        element.clear();
-        element.sendKeys(value);
+        return driver.findElement(reserveMonth).getAttribute("value");
     }
     
     public String getReserveDay() {
-        return driver.findElement(reserveDay).getText();
+        return driver.findElement(reserveDay).getAttribute("value");
     }
 
     public void setReserveDate(String year, String month, String day) {
-        setReserveYear(year);
-        setReserveMonth(month);
-        setReserveDay(day);
+        WebElement element = driver.findElement(datePick);
+        element.clear();
+        element.sendKeys(year + "/" + month + "/" + day);
     }
 
     public void setReserveTerm(String value) {
-        WebElement element = driver.findElement(reserveTerm);
-        element.clear();
-        element.sendKeys(value);
+        Select select = new Select(driver.findElement(reserveTerm));
+        select.selectByValue(value);
     }
     
     public String getReserveTerm() {
-        return driver.findElement(reserveTerm).getText();
+        return driver.findElement(reserveTerm).getAttribute("value");
     }
     
     public void setHeadCount(String value) {
-        WebElement element = driver.findElement(headCount);
-        element.clear();
-        element.sendKeys(value);
+        Select select = new Select(driver.findElement(headCount));
+        select.selectByValue(value);
     }
     
     public String getHeadCount() {
-        return driver.findElement(headCount).getText();
+        return driver.findElement(headCount).getAttribute("value");
     }
     
     public void setBreakfast(boolean on) {
@@ -113,146 +95,16 @@ public class ReserveInputPage {
         return driver.findElement(planB).isSelected();
     }
 
-    public void setReservePersonName(String value) {
-        WebElement element = driver.findElement(reservePersonName);
+    public void setGuestName(String value) {
+        WebElement element = driver.findElement(guestName);
         element.clear();
         element.sendKeys(value);
     }
     
-    public String getReservePersonName() {
-        return driver.findElement(reservePersonName).getText();
+    public String getGuestName() {
+        return driver.findElement(guestName).getAttribute("value");
     }
-    package practicework.pages;
-
-    import org.openqa.selenium.By;
-    import org.openqa.selenium.WebDriver;
-    import org.openqa.selenium.WebElement;
-
-    public class ReserveInputPage {
-        private WebDriver driver;
-        private By reserveYear = By.id("reserve_year");
-        private By reserveMonth = By.id("reserve_month");
-        private By reserveDay = By.id("reserve_day");
-        private By reserveTerm = By.id("reserve_term");
-        private By headCount = By.id("headcount");
-        private By breakfastOn = By.id("breakfast_on");
-        private By breakfastOff = By.id("breakfast_off");
-        private By planA = By.id("plan_a");
-        private By planB = By.id("plan_b");
-        private By guestName = By.id("guestname");
-        private By goToNext = By.id("goto_next");
-
-        public ReserveInputPage(WebDriver driver) {
-            this.driver = driver;
-        }
-
-        public void setReserveYear(String value) {
-            WebElement element = driver.findElement(reserveYear);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getReserveYear() {
-            return driver.findElement(reserveYear).getAttribute("value");
-        }
-
-        public void setReserveMonth(String value) {
-            WebElement element = driver.findElement(reserveMonth);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getReserveMonth() {
-            return driver.findElement(reserveMonth).getAttribute("value");
-        }
-        
-        public void setReserveDay(String value) {
-            WebElement element = driver.findElement(reserveDay);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getReserveDay() {
-            return driver.findElement(reserveDay).getAttribute("value");
-        }
-
-        public void setReserveDate(String year, String month, String day) {
-            setReserveYear(year);
-            setReserveMonth(month);
-            setReserveDay(day);
-        }
-
-        public void setReserveTerm(String value) {
-            WebElement element = driver.findElement(reserveTerm);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getReserveTerm() {
-            return driver.findElement(reserveTerm).getAttribute("value");
-        }
-        
-        public void setHeadCount(String value) {
-            WebElement element = driver.findElement(headCount);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getHeadCount() {
-            return driver.findElement(headCount).getAttribute("value");
-        }
-        
-        public void setBreakfast(boolean on) {
-            if (on) {
-                driver.findElement(breakfastOn).click();
-            } else {
-                driver.findElement(breakfastOff).click();            
-            }
-        }
-        
-        public boolean getBreakfast() {
-            String checked = driver.findElement(breakfastOn).getAttribute("checked");
-            return "true".equals(checked);
-        }
-        
-        public void setPlanA(boolean checked) {
-            WebElement element = driver.findElement(planA);
-            if (element.isSelected() != checked) {
-                element.click();
-            }
-        }
-        
-        public boolean getPlanA() {
-            return driver.findElement(planA).isSelected();
-        }
-
-        public void setPlanB(boolean checked) {
-            WebElement element = driver.findElement(planB);
-            if (element.isSelected() != checked) {
-                element.click();
-            }
-        }
-
-        public boolean getPlanB() {
-            return driver.findElement(planB).isSelected();
-        }
-        
-        public void setGuestName(String value) {
-            WebElement element = driver.findElement(guestName);
-            element.clear();
-            element.sendKeys(value);
-        }
-        
-        public String getGuestName() {
-            return driver.findElement(guestName).getAttribute("value");
-        }
-        
-        public ReserveConfirmPage goToNext() {
-            driver.findElement(goToNext).click();
-            return new ReserveConfirmPage(driver);
-        }
-    }
-
+    
     public ReserveConfirmPage goToNext() {
         driver.findElement(goToNext).click();
         return new ReserveConfirmPage(driver);
